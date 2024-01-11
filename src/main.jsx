7f1a72f610e582f5183117c858fbffbbe62fed50
lucianocabelloo <you@example.com>
2024-01-11 15:11:25 -0300

    Add Class Usuario

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import '@fontsource/inter';
+import { uuid } from 'uuidv4';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
