6d55e2cc80228eacfeacd175af12b795320de1f6
lucianocabelloo <you@example.com>
2024-01-12 06:21:44 -0300

    Add React hook Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import FormUser from "./components/FormUser"
 
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
 
   return (
     <>
-
+    <FormUser/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import '@fontsource/inter';
-import { uuid } from 'uuidv4';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
